6f151fac1e9e69437a9a903fe5bb3932c82c2a18
etEthiopia <daginegussu@gmail.com>
2020-01-09 08:17:24 +0300

    change text hopefully

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [outputText, setOutputText] = useState("Open up App.js");
   return (
     <View style={styles.container}>
-      <Text> APP.js changed! </Text>
+      <Text> {outputText} </Text>
+      <Button
+        title="Change Text"
+        onPress={() => setOutputText("The text has changed")}
+      />
     </View>
   );
 }
